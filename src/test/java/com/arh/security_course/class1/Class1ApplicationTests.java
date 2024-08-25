c89c7ba14d73058a994283b9b1671f684862b843
Alencar Hentges <alencarhentges@gmail.com>
2024-08-24 22:54:00 -0300

    first and second class

---
+++ src/test/java/com/arh/security_course/class1/Class1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arh.security_course.class1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
